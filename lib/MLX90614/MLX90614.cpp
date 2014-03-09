2f2ff4ddb4b563bf554c345c7fd422f51b05ba52
Chris Ramsay <chris@ramsay-family.net>
2014-03-09 16:28:26 +0000

    License and other items

--- lib/MLX90614/MLX90614.cpp
+++ lib/MLX90614/MLX90614.cpp
@@ -1,3 +1,31 @@
+/*
+    This file is part of the arduinobits library.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    ***************************************************************************
+
+    This forms the main class for enabling the Arduino to read data from
+    the Melexis MLX90614 Infra Red thermometer for non contact temperature 
+    measurements.
+
+    Many items herein originated in Lara Booth's 2012 NanoSatisfi library.
+    https://github.com/ArduSat/video-tutorials/tree/master/NanoSatisfi_MLX90614
+    I have added a little more functionality where required
+
+    ***************************************************************************
+*/
 #include "MLX90614.h"
 
 //constructor
@@ -31,23 +59,24 @@ double MLX90614::getReading(int address, unsigned char ttype)
   data_high = Wire.read();
   // Read checksum 
   pec = Wire.read();
-     
-  Wire.endTransmission(); //terminate transmission
+  // Terminate transmission
+  Wire.endTransmission();
      
   // This masks off the error bit of the high byte, then moves it left 8 bits and adds the low byte.
   // Taken from bildr forum on MLX90614
   tempData = (double) (((data_high & 0x007F) << 8) + data_low);
-  return (tempData * factor) - 0.01; //multiply by resolution and account for error to convert to Kelvin
+  // Multiply by resolution and account for error to convert to Kelvin
+  return (tempData * factor) - 0.01;
 }
 
-//returns object temperature in degrees Celsius
+// Returns object temperature in degrees Celsius
 float MLX90614::getObjTemp(int address)
 {
-  return (float) getReading(address, TEMP_OBJ) - 273.15; //convert from Kelvin to Celsius
+  return (float) getReading(address, TEMP_OBJ) - 273.15;
 }
 
-//returns ambient temperature in degrees Celsius
+// Returns ambient temperature in degrees Celsius
 float MLX90614::getAmbTemp(int address)
 {
-  return (float) getReading(address, TEMP_AMB) - 273.15; //convert from Kelvin to Celsius
+  return (float) getReading(address, TEMP_AMB) - 273.15;
 }
--- lib/MLX90614/MLX90614.h
+++ lib/MLX90614/MLX90614.h
@@ -1,8 +1,31 @@
 /*
-  Morse.h - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
+    This file is part of the arduinobits library.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    ***************************************************************************
+
+    This forms header enabling the Arduino to read data from the Melexis
+    MLX90614 Infra Red thermometer for non contact temperature measurements.
+
+    Many items herein originated in Lara Booth's 2012 NanoSatisfi library.
+    https://github.com/ArduSat/video-tutorials/tree/master/NanoSatisfi_MLX90614
+    I have added a little more functionality where required
+
+    ***************************************************************************
 */
+
 #ifndef MLX90614_h
 #define MLX90614_h
 
